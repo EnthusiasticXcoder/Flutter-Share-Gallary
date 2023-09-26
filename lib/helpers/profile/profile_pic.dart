67f93ddcc46d49f7a6c1f4fc30d6dc330111c5f3
Anshul Verma <anshulverma1405@gmail.com>
2023-09-26 15:15:30 +0530

    added show profilw pic dialog claass

--- lib/helpers/profile/profile_pic.dart
+++ lib/helpers/profile/profile_pic.dart
@@ -1,11 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:gallary/helpers/profile/show_image_picker.dart';
+import 'package:gallary/services/cloud/bloc/bloc.dart';
 
-class ProfilePicture extends StatelessWidget {
+class ProfilePicture {
   final String? imagePath;
-  const ProfilePicture({super.key, this.imagePath});
 
-  @override
-  Widget build(BuildContext context) {
+  ProfilePicture.showDialog(
+    BuildContext context,
+    this.imagePath,
+    IconData child, {
+    required VoidCallback onDeletePic,
+    required VoidCallback oncameraPic,
+    required VoidCallback onGallaryPic,
+  }) {
+    showDialog(
+      context: context,
+      builder: (_) =>
+          build(context, child, oncameraPic, onGallaryPic, onDeletePic),
+    );
+  }
+
+  ProfilePicture.showImageView(
+      BuildContext context, this.imagePath, IconData child) {
+    showDialog(
+      context: context,
+      builder: (_) => imagebuild(context, child),
+    ).then((_) => context.read<CloudBloc>().add(
+          const CloudEventInitial(),
+        ));
+  }
+
+  Widget imagebuild(BuildContext context, IconData child) {
+    return GestureDetector(
+      onTap: () => Navigator.pop(context),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            margin: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.05),
+            decoration: BoxDecoration(
+              color: (imagePath == null) ? Colors.white70 : Colors.black54,
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.34,
+              width: double.maxFinite,
+              child: (imagePath == null)
+                  ? Icon(
+                      child,
+                      size: MediaQuery.sizeOf(context).width * 0.6,
+                    )
+                  : Image.network(
+                      imagePath!,
+                      fit: BoxFit.scaleDown,
+                      errorBuilder: (context, error, stackTrace) =>
+                          const Text('Unable To Load Image'),
+                    ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(
+    BuildContext context,
+    IconData child,
+    VoidCallback oncameraPic,
+    VoidCallback onGallaryPic,
+    VoidCallback onDeletePic,
+  ) {
     return GestureDetector(
       onTap: () {
         Navigator.of(context).pop();
@@ -14,25 +81,30 @@ class ProfilePicture extends StatelessWidget {
           backgroundColor: Colors.transparent,
           body: Center(
             child: Container(
-              height: 350,
-              margin: const EdgeInsets.symmetric(horizontal: 12.0),
+              height: MediaQuery.of(context).size.height * 0.4,
+              margin: EdgeInsets.symmetric(
+                  horizontal: MediaQuery.of(context).size.width * 0.05),
               decoration: BoxDecoration(
-                  color: Colors.white70,
+                  color: Colors.black54,
                   borderRadius: BorderRadius.circular(24)),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(24),
-                child: Stack(
-                  alignment: AlignmentDirectional.bottomCenter,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
                     // Image
-                    Center(
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.34,
+                      width: double.maxFinite,
                       child: (imagePath == null)
-                          ? const Text(
-                              'No Profile Image Found\nUpload A Image',
-                              textAlign: TextAlign.center,
+                          ? Icon(
+                              child,
+                              color: Colors.white,
+                              size: MediaQuery.sizeOf(context).width * 0.6,
                             )
                           : Image.network(
                               imagePath!,
+                              fit: BoxFit.scaleDown,
                               errorBuilder: (context, error, stackTrace) =>
                                   const Text('Unable To Load Image'),
                             ),
@@ -40,9 +112,20 @@ class ProfilePicture extends StatelessWidget {
                     // Edit Button
                     Container(
                       width: double.infinity,
+                      height: MediaQuery.of(context).size.height * 0.06,
                       color: Colors.white,
                       child: TextButton.icon(
-                          onPressed: () {},
+                          // edit profile pic
+                          onPressed: () async {
+                            Navigator.pop(context);
+                            // show image picker bottom dialog sheet
+                            await showImagePicker(
+                              context: context,
+                              onDeletePic: onDeletePic,
+                              oncameraPic: oncameraPic,
+                              onGallaryPic: onGallaryPic,
+                            );
+                          },
                           icon: const Icon(Icons.edit),
                           label: const Text('Edit')),
                     )
