954d06689624da8ed7e536dc4dd4e6ecf95829b0
Anshul Verma <anshulverma1405@gmail.com>
2023-09-26 15:16:26 +0530

    refactor imports

--- lib/pages/auth/view/forgot_password_view.dart
+++ lib/pages/auth/view/forgot_password_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gallary/pages/auth/auth.dart';
-import 'package:gallary/services/auth/bloc/auth_bloc.dart';
+import 'package:gallary/pages/auth/widget/widget.dart';
+import 'package:gallary/services/auth/auth.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   final List<String> mail = [''];
+  final _formKey = GlobalKey<FormState>();
   ForgotPasswordView({super.key});
 
   @override
@@ -14,7 +15,7 @@ class ForgotPasswordView extends StatelessWidget {
       title: 'Reset Password',
       onBack: () {
         context.read<AuthBloc>().add(
-              const AuthEventShouldSignIn(),
+              const AuthEventSignIn(),
             );
       },
       label:
@@ -22,39 +23,44 @@ class ForgotPasswordView extends StatelessWidget {
       child: WillPopScope(
         onWillPop: () {
           context.read<AuthBloc>().add(
-                const AuthEventShouldSignIn(),
+                const AuthEventSignIn(),
               );
           return Future.value(false);
         },
-        child: Column(
-          children: [
-            TextInputField(
-              label: 'Enter Your Email',
-              onChange: (value) {
-                mail.removeAt(0);
-                mail.insert(0, value!);
-              },
-            ),
-
-            // margin
-            SizedBox(height: MediaQuery.of(context).size.height * 0.03),
-            // Send mail Button
-            ElevatedButton(
-                onPressed: () {
-                  context.read<AuthBloc>().add(
-                        AuthEventForgotPassword(email: mail.firstOrNull),
-                      );
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextInputField(
+                label: 'Enter Your Email',
+                onChange: (value) {
+                  mail.removeAt(0);
+                  mail.insert(0, value!);
                 },
-                style: ElevatedButton.styleFrom(
-                    textStyle: const TextStyle(fontSize: 16),
-                    minimumSize: Size(double.maxFinite,
-                        MediaQuery.of(context).size.height * 0.055),
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16.0))),
-                child: const Text('Send Password Reset Mail')),
-          ],
+              ),
+
+              // margin
+              SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+              // Send mail Button
+              ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      context.read<AuthBloc>().add(
+                            AuthEventForgotPassword(email: mail.first),
+                          );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 16),
+                      minimumSize: Size(double.maxFinite,
+                          MediaQuery.of(context).size.height * 0.055),
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0))),
+                  child: const Text('Send Password Reset Mail')),
+            ],
+          ),
         ),
       ),
     );
