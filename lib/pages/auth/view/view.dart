81f8cdd805efdf3af4825c1073770699d641d56f
Anshul Verma <anshulverma1405@gmail.com>
2023-09-28 15:33:30 +0530

    added exports

--- lib/pages/auth/view/view.dart
+++ lib/pages/auth/view/view.dart
@@ -4,3 +4,5 @@ export 'signin_view.dart';
 export 'signup_view.dart';
 export 'verification_view.dart';
 export 'welcome_view.dart';
+export 'google_signin.dart';
+export 'forgot_password_view.dart';
