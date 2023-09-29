f4bc38fe8d13f20bd55d666768cd44ba4ac7cff4
Anshul Verma <anshulverma1405@gmail.com>
2023-09-29 15:25:27 +0530

    added group info appbar

--- lib/pages/group/views/group_view.dart
+++ lib/pages/group/views/group_view.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gallary/helpers/image/image_grid.dart';
 import 'package:gallary/routs/app_routs.dart';
-import 'package:gallary/services/cloud/bloc/bloc.dart';
+
+import '../../../services/cloud/bloc/bloc.dart';
+import '../widgets/widgets.dart';
 
 class GroupView extends StatelessWidget {
   final String groupId;
@@ -12,7 +14,16 @@ class GroupView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlue.shade400,
-      appBar: GroupInfoAppBar(groupId: groupId),
+      appBar: GroupInfoAppBar(
+        stream: context.select((CloudBloc bloc) => bloc.getGroupData(groupId)),
+        groupId: groupId,
+        onPress: () {
+          Navigator.of(context).pushNamed(
+            AppRouts.groupDetailsPage,
+            arguments: groupId,
+          );
+        },
+      ),
       body: Container(
         padding: const EdgeInsets.symmetric(
           horizontal: 10,
@@ -34,72 +45,4 @@ class GroupView extends StatelessWidget {
       ),
     );
   }
-}
-
-class GroupInfoAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const GroupInfoAppBar({
-    super.key,
-    required this.groupId,
-  });
-
-  final String groupId;
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      titleSpacing: 0,
-      leading: TextButton(
-        onPressed: () => Navigator.pop(context),
-        style: TextButton.styleFrom(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0)),
-        child: const Row(
-          children: <Widget>[
-            BackButtonIcon(),
-            Hero(
-              tag: 'CircularProfileIcon',
-              child: CircleAvatar(
-                child: Icon(Icons.group),
-              ),
-            )
-          ],
-        ),
-      ),
-      leadingWidth: 80,
-      title: StreamBuilder<GroupData>(
-          stream:
-              context.select((CloudBloc bloc) => bloc.getGroupData(groupId)),
-          builder: (context, snapshot) {
-            return ListTile(
-              shape: const StadiumBorder(),
-              onTap: () {
-                Navigator.of(context).pushNamed(
-                  AppRouts.groupDetailsPage,
-                  arguments: groupId,
-                );
-              },
-              title: Text(
-                snapshot.data?.name ?? '',
-                style: const TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
-              ),
-              subtitle: Text(
-                snapshot.data?.info ?? '',
-                style: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                ),
-              ),
-            );
-          }),
-      toolbarHeight: 90,
-      iconTheme: const IconThemeData(size: 30, color: Colors.white),
-      backgroundColor: Colors.transparent,
-    );
-  }
-
-  @override
-  Size get preferredSize => const Size(double.maxFinite, 90);
 }
---
+++ lib/pages/group/widgets/group_info_appbar.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:gallary/services/cloud/bloc/cloud_bloc.dart';
+
+class GroupInfoAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const GroupInfoAppBar({
+    super.key,
+    required this.groupId,
+    required this.onPress,
+    required this.stream,
+  });
+
+  final Stream<GroupData> stream;
+  final VoidCallback onPress;
+  final String groupId;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      titleSpacing: 0,
+      leading: TextButton(
+        onPressed: () => Navigator.pop(context),
+        style: TextButton.styleFrom(
+            padding: const EdgeInsets.symmetric(horizontal: 5.0)),
+        child: const Row(
+          children: <Widget>[
+            BackButtonIcon(),
+            Hero(
+              tag: 'CircularProfileIcon',
+              child: CircleAvatar(
+                child: Icon(Icons.group),
+              ),
+            )
+          ],
+        ),
+      ),
+      leadingWidth: 80,
+      title: StreamBuilder<GroupData>(
+          stream: stream,
+          builder: (context, snapshot) {
+            return ListTile(
+              shape: const StadiumBorder(),
+              onTap: onPress,
+              title: Text(
+                snapshot.data?.name ?? '',
+                style: const TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                snapshot.data?.info ?? '',
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }),
+      toolbarHeight: 90,
+      iconTheme: const IconThemeData(size: 30, color: Colors.white),
+      backgroundColor: Colors.transparent,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size(double.maxFinite, 90);
+}
